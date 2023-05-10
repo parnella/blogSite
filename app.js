34dd548eb1ccfc282b4412d95a609a61e3e55ad3
Alycia Parnell <alycia@alycias-air.lan>
2023-05-09 21:28:15 -0600

    Add pages

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+
+const posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,7 +20,7 @@ app.use(express.static("public"));
 app.use(express.static("views/partials"));
 
 app.get("/",function(req,res) {
-  res.render("home", {content: homeStartingContent})
+  res.render("home", {content: homeStartingContent, posts: posts})
 });
 
 app.get("/about",function(req,res) {
@@ -32,10 +35,27 @@ app.get("/compose",function(req,res) {
   res.render("compose")
 });
 
+app.get("/posts/:postName",function(req,res) {
+  const requestedTitle = req.params.postName;
+  let match = 0;
+
+  posts.forEach(function(post) {
+    if (_.lowerCase(post.title) === _.lowerCase(requestedTitle)) {
+      console.log("match found");    }
+  })
+
+  res.render("post", {postTitle: req.params.title})
+});
+
 app.post("/compose",function(req,res) {
-  console.log(req.body.postTitle);
-  
-  res.render("home", {content: homeStartingContent});
+  let post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/posts/" + post.title);
 })
 
 
